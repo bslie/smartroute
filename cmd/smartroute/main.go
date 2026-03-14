0e038b72382d1b87e68ef57fb2ba9700e1031060
Lie <krovlin97@yandex.ru>
2026-03-14 12:32:25 +0300

    fix: stop ignoring cmd/smartroute, add entrypoint to repo

---
+++ cmd/smartroute/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/smartroute/smartroute/internal/cli"
+)
+
+var version = "0.1.0"
+
+func main() {
+	cli.RootCmd.Version = version
+	if err := cli.RootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
